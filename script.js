bb716dc7ece5727cda93e94c4d4a10edb2f46200
Robert <112430289+robertvieiraa@users.noreply.github.com>
2026-03-06 21:49:38 -0300

    Pré-visualização de imagem

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-const carregarImg = document.getElementById('carregar-imagem')
-const botaoCarregar = document.getElementById('botao-carregar')
+const uploadBtn = document.getElementById('upload-btn')
+const inputUpload = document.getElementById('image-upload')
 
-botaoCarregar.addEventListener('click', () => {
-    carregarImg.click()
+uploadBtn.addEventListener('click', () => {
+    inputUpload.click()
+})
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader()
+        leitor.onload = () => {
+            resolve( {url: leitor.result, nome: arquivo.name} )
+        }
+        leitor.onerror = () => {
+            reject(`O arquivo ${arquivo.name} não pôde ser lido.`)
+        }
+        leitor.readAsDataURL(arquivo)
+    })
+}
+
+const imagemPrincipal = document.querySelector('.main-imagem')
+const nomeDaImagem = document.querySelector('.container-nome-imagem p')
+
+inputUpload.addEventListener('change', async (evento) => {
+    const arquivo = evento.target.files[0]
+    if (arquivo) {
+        try {
+            const conteudoDoArquivo = await lerConteudoDoArquivo(arquivo)
+            imagemPrincipal.src = conteudoDoArquivo.url
+            nomeDaImagem.textContent = conteudoDoArquivo.nome
+        } catch (erro) {
+            console.error('Houve um erro ao carregar o arquivo.')
+        }
+    }
 })
